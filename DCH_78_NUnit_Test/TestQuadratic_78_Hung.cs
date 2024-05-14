0a9b60ff97a0bcd0a19779de16f03318848c862b
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2024-05-15 02:16:44 +0700

    Fix TestCase for NUnit

--- DCH_78_NUnit_Test/TestQuadratic_78_Hung.cs
+++ DCH_78_NUnit_Test/TestQuadratic_78_Hung.cs
@@ -8,7 +8,92 @@ namespace NUnit_Test_78_Hung
     public class TestQuadratic_78_Hung
     {
         public TestContext TestContext { get; set; }
-         
+
+
+        [TestMethod]
+        public void TC1_MotNghiem_78_Hung()
+        {
+            double a_78_Hung = 0;
+            double b_78_Hung = 2;
+            double c_78_Hung = 3;
+            string expected_78_Hung = "Phuong trinh co mot nghiem x = -1.5";
+
+            Quadratic_78_Hung quad_78_Hung = new Quadratic_78_Hung(a_78_Hung, b_78_Hung, c_78_Hung);
+
+            string result_78_Hung = quad_78_Hung.Solve_78_Hung();
+            Assert.AreEqual(result_78_Hung, expected_78_Hung);
+        }
+
+        [TestMethod]
+        public void TC2_VoNghiem_78_Hung()
+        {
+            double a_78_Hung = 0;
+            double b_78_Hung = 0;
+            double c_78_Hung = 2;
+            string expected_78_Hung = "Phuong trinh vo nghiem";
+
+            Quadratic_78_Hung quad_78_Hung = new Quadratic_78_Hung(a_78_Hung, b_78_Hung, c_78_Hung);
+
+            string result_78_Hung = quad_78_Hung.Solve_78_Hung();
+            Assert.AreEqual(result_78_Hung, expected_78_Hung);
+        }
+
+        [TestMethod]
+        public void TC3_VoSoNghiem_78_Hung()
+        {
+            double a_78_Hung = 0;
+            double b_78_Hung = 0;
+            double c_78_Hung = 0;
+            string expected_78_Hung = "Phuong trinh vo so nghiem";
+
+            Quadratic_78_Hung quad_78_Hung = new Quadratic_78_Hung(a_78_Hung, b_78_Hung, c_78_Hung);
+
+            string result_78_Hung = quad_78_Hung.Solve_78_Hung();
+            Assert.AreEqual(result_78_Hung, expected_78_Hung);
+        }   
+
+        [TestMethod]
+        public void TC4_NghiemPhanBiet_78_Hung()
+        {
+            double a_78_Hung = 1;
+            double b_78_Hung = -3;
+            double c_78_Hung = 2;
+            string expected_78_Hung = "Phuong trinh co hai nghiem phan biet x1 = 2 x2 = 1";
+
+            Quadratic_78_Hung quad_78_Hung = new Quadratic_78_Hung(a_78_Hung, b_78_Hung, c_78_Hung);
+
+            string result_78_Hung = quad_78_Hung.Solve_78_Hung();
+            Assert.AreEqual(result_78_Hung, expected_78_Hung);
+        }
+
+        [TestMethod]
+        public void TC5_NghiemKep_78_Hung()
+        {
+            double a_78_Hung = 1;
+            double b_78_Hung = -2;
+            double c_78_Hung = 1;
+            string expected_78_Hung = "Phuong trinh co nghiem kep x = 1";
+
+            Quadratic_78_Hung quad_78_Hung = new Quadratic_78_Hung(a_78_Hung, b_78_Hung, c_78_Hung);
+
+            string result_78_Hung = quad_78_Hung.Solve_78_Hung();
+            Assert.AreEqual(result_78_Hung, expected_78_Hung);
+        }
+
+        [TestMethod]
+        public void TC6_VoNghiem_78_Hung()
+        {
+            double a_78_Hung = 1;
+            double b_78_Hung = 0;
+            double c_78_Hung = 1;
+            string expected_78_Hung = "Phuong trinh vo nghiem";
+
+            Quadratic_78_Hung quad_78_Hung = new Quadratic_78_Hung(a_78_Hung, b_78_Hung, c_78_Hung);
+
+            string result_78_Hung = quad_78_Hung.Solve_78_Hung();
+            Assert.AreEqual(result_78_Hung, expected_78_Hung);
+        }
+
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\DataUnitTestQuadratic.csv",
             "DataUnitTestQuadratic#csv", DataAccessMethod.Sequential)] 
         [TestMethod]
